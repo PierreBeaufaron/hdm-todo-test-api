57bbbaf07c45b0d71c10a598f5d32a451248c3a6
Pierre Beaufaron <p.beaufaron@gmail.com>
2024-12-12 17:10:20 +0100

    Commit back ok avant zèle.

--- src/UseCase/SaveTask/SaveTaskUseCase.ts
+++ src/UseCase/SaveTask/SaveTaskUseCase.ts
@@ -1,16 +1,32 @@
-import { Injectable } from '@nestjs/common';
-import { Task } from '@prisma/client';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { Prisma, Task } from '@prisma/client';
 import { UseCase } from '../../index';
 import SaveTaskDto from './SaveTaskDto';
+import TaskRepository from 'src/Repositories/TaskRepository';
 
 @Injectable()
 export default class SaveTaskUseCase implements UseCase<Promise<Task>, [dto: SaveTaskDto]> {
-  constructor() {}
+  constructor(private readonly taskRepository: TaskRepository) {}
 
   async handle(dto: SaveTaskDto) {
-    /*
-    * @todo IMPLEMENT HERE : VALIDATION DTO, DATA SAVING, ERROR CATCHING
-     */
+    const { id, ...data } = dto;
+
+    if(!id) {
+      try {
+        return await this.taskRepository.save(data)     
+      } catch (error) {
+        throw new BadRequestException(error.message);
+      }
+    }
+    try {
+      return this.taskRepository.save({
+        ...data,
+        id,
+      });
+    } catch (error) {
+      
+    }
+    
 
     return null;
   }
